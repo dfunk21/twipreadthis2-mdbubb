8912053c0bf567793e0031dfbc9944134447b9f3
mb977 <mdb17@thebubbs.net>
2016-11-14 10:33:27 -0500

    adfg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import sun.tools.java.Imports;
+
+import java.io.File;
+import java.io.*;
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 	// write your code here
+        Scanner txtFileThing = new Scanner(new File("Imports.txt"));
+       int maxIN = -1;
+        String text[] = new String[1000];
+        while(txtFileThing.hasNext()){
+            maxIN++;
+            text[maxIN] = txtFileThing.next();
+            System.out.println(text[1]);
+        }
+
+
     }
 }
